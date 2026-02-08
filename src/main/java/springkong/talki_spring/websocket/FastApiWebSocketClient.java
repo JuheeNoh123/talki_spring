739b2d9687a2e638a7831ccb685ef077a64eaa6f
juheeNoh123 <juhee10131013@gmail.com>
2026-02-09 01:55:08 +0900

    fix ws uri ngrok

--- src/main/java/springkong/talki_spring/websocket/FastApiWebSocketClient.java
+++ src/main/java/springkong/talki_spring/websocket/FastApiWebSocketClient.java
@@ -15,7 +15,7 @@ public class FastApiWebSocketClient {
         ReactorNettyWebSocketClient client = new ReactorNettyWebSocketClient();
 
         URI uri = URI.create(
-                "ws://182.209.235.80:8000/realtime?type=" + presentationType
+                "ws://curelessly-unusable-jolanda.ngrok-free.dev:8000/realtime?type=" + presentationType
         );
 
         return client.execute(uri, session -> {
