49df2ab9399786fe7af72e34fb2acdbfc2a4de22
juheeNoh123 <juhee10131013@gmail.com>
2026-02-09 03:58:41 +0900

    fix : test

--- src/main/java/springkong/talki_spring/controller/dataController.java
+++ src/main/java/springkong/talki_spring/controller/dataController.java
@@ -6,6 +6,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.reactive.function.client.WebClient;
 import springkong.talki_spring.dto.FeedbackEvent;
 import springkong.talki_spring.dto.Test;
 import springkong.talki_spring.service.AnalyzeService;
@@ -18,9 +19,15 @@ import java.util.List;
 public class dataController {
     private final FeedbackService feedbackService;
     private final AnalyzeService analyzeService;
+    private final WebClient fastApiWebClient;
     @GetMapping("/analyze/get")
     public String testGet() {
-        return "GET OK";
+        return fastApiWebClient
+                .get()                           // ✅ GET
+                .uri("/health")                  // ✅ FastAPI health
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
     }
 
     @PostMapping("/analyze/get")
