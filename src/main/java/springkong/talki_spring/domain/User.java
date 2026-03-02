d6fe9c1b45f2fa6f01331325d9ece4a73f760110
juheeNoh123 <juhee10131013@gmail.com>
2026-03-02 14:53:10 +0900

    fix dev.yml

--- src/main/java/springkong/talki_spring/domain/User.java
+++ src/main/java/springkong/talki_spring/domain/User.java
@@ -9,6 +9,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
