45159c78221cbe293cd5f865b56fb6052ea889a4
juheeNoh123 <juhee10131013@gmail.com>
2026-03-04 04:47:06 +0900

    security fixed_v7

--- src/main/java/springkong/talki_spring/config/SecurityConfig.java
+++ src/main/java/springkong/talki_spring/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -39,6 +40,7 @@ public class SecurityConfig {
     }
 
     @Bean
+    @Order(0)
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .cors(Customizer.withDefaults()) // spring boot의 기본 CORS설정 사용
