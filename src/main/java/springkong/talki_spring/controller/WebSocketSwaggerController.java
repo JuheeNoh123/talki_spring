06130248476c447474ab2a4a353377ed3c3d7774
juheeNoh123 <juhee10131013@gmail.com>
2026-03-13 02:48:20 +0900

    fix: ãswagger

--- src/main/java/springkong/talki_spring/controller/WebSocketSwaggerController.java
+++ src/main/java/springkong/talki_spring/controller/WebSocketSwaggerController.java
@@ -27,23 +27,38 @@ Example
 
 ws://43.201.182.246:8080/realtime?type=large
 
+
 초기 연결 시 응답 예시
+
 SERVER: {"type": "session_start", "presentationId": "94b364fe919044818558bf83665a8f2f"}
 
+
 실시간 발표 진행 중 요청/응답 예시
+
 Client → Server Message
+
 {
+
   "face": { "468": {"x":0.2,"y":0.4} },
+  
   "pose": { "13": {"x":0.5,"y":0.5} },
+  
   "audio": "BASE64_AUDIO",
+  
   "timestamp": 1712345678123
+  
 }
 
 Server → Client Message
+
 {
+
   "type": "feedback",
+  
   "raw_result": {gaze:{...}, pose_detected:{...}, pose_landmarks:{...}, ...},
+  
   "data": "시선이 불안정합니다." / null
+  
 }
 """
     )
