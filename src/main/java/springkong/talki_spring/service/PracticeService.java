a4f604f2682432d102c77e4d713320f0da7f28fc
juheeNoh123 <juhee10131013@gmail.com>
2026-03-17 16:55:51 +0900

    fix: practice ㅕuser

--- src/main/java/springkong/talki_spring/service/PracticeService.java
+++ src/main/java/springkong/talki_spring/service/PracticeService.java
@@ -7,6 +7,7 @@ import springkong.talki_spring.domain.Practice;
 import springkong.talki_spring.domain.User;
 import springkong.talki_spring.dto.request.PracticeDTO;
 import springkong.talki_spring.enums.PracticeType;
+import springkong.talki_spring.exception.NotFoundException;
 import springkong.talki_spring.repository.PracticeRepository;
 import springkong.talki_spring.repository.UserRepository;
 
@@ -14,11 +15,14 @@ import springkong.talki_spring.repository.UserRepository;
 @RequiredArgsConstructor
 public class PracticeService {
     private final PracticeRepository practiceRepository;
+    private final UserRepository userRepository;
 
     @Transactional
     public void endPracticeAndSave(User user, PracticeDTO.PracticeDTOBuilder dtoBuilder){
+        User persistedUser = userRepository.findById(user.getId())
+                .orElseThrow(() -> new NotFoundException("존재하지 않는 사용자 입니다."));
         for (PracticeType practiceType:dtoBuilder.getPracticeType()){
-            Practice practice = new Practice(user,
+            Practice practice = new Practice(persistedUser,
                     practiceType,
                     dtoBuilder.getThoughtRecognition(),
                     dtoBuilder.getBehavioralTestResults(),
